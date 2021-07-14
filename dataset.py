2d46c7f8a1ec3044c6cf53f2b038d99f4b6e6646
Guilherme G. Schardong <schardong.gg@gmail.com>
2021-07-14 17:32:06 -0300

    Dynamically sampling points in the domain when idx is out of range.
    * Copied and split the sampling functions from `utils.py` to
     datasets.py
    
    * Keeping the SurfacePointCloud instance with the acceleration
      structure in our PointCloud for quickly querying new samples' SDF
      values
    
    * Added a scaling option to the PointCloud class.

--- dataset.py
+++ dataset.py
@@ -1,10 +1,69 @@
 # coding: utf-8
 
-from mesh_to_sdf import scale_to_unit_sphere
+from mesh_to_sdf.surface_point_cloud import SurfacePointCloud
+from mesh_to_sdf import (get_surface_point_cloud, scale_to_unit_cube,
+                         scale_to_unit_sphere)
+import numpy as np
 import trimesh
 import torch
 from torch.utils.data import Dataset
-from util import sample_mesh
+# from util import sample_mesh
+
+
+def _sample_on_surface(mesh: trimesh.Trimesh,
+                       n_points: int,
+                       sample_vertices=True):
+    if sample_vertices:
+        idx = np.random.choice(
+            np.arange(start=0, stop=len(mesh.vertices)),
+            size=n_points,
+            replace=False
+        )
+        on_points = mesh.vertices[idx]
+        on_normals = mesh.vertex_normals[idx]
+    else:
+        on_points, face_idx = mesh.sample(
+            count=n_points,
+            return_index=True
+        )
+        on_normals = mesh.face_normals[face_idx]
+
+    return torch.from_numpy(np.hstack((
+        on_points,
+        on_normals,
+        np.zeros((n_points, 1))
+    )).astype(np.float32))
+
+
+def _sample_domain(point_cloud: SurfacePointCloud,
+                   n_points: int,
+                   balance_in_out_points=False):
+    domain_points = np.random.uniform(-1, 1, size=(n_points, 3))
+    domain_sdf, domain_normals = point_cloud.get_sdf(
+        domain_points,
+        use_depth_buffer=False,
+        return_gradients=True
+    )
+    if balance_in_out_points:
+        in_surf = domain_sdf < 0
+        domain_sdf = np.concatenate((
+            domain_sdf[in_surf],
+            domain_sdf[~in_surf][:sum(in_surf)]), axis=0
+        )
+        domain_points = np.vstack((
+            domain_points[in_surf, :],
+            domain_points[~in_surf, :][:sum(in_surf)]
+        ))
+        domain_normals = np.vstack((
+            domain_normals[in_surf, :],
+            domain_normals[~in_surf, :][:sum(in_surf)]
+        ))
+
+    return torch.from_numpy(np.hstack((
+        domain_points,
+        domain_normals,
+        domain_sdf[:, np.newaxis]
+    )).astype(np.float32))
 
 
 class PointCloud(Dataset):
@@ -18,8 +77,10 @@ class PointCloud(Dataset):
     samples_on_surface: int
         Number of surface samples to fetch (i.e. {X | f(X) = 0}).
 
-    samples_off_surface: int
-        Number of samples with f(X) != 0 to fetch.
+    scaling: str or None, optional
+        The scaling to apply to the mesh. Possible values are: None
+        (no scaling), "bbox" (-1, 1 in all axes), "sphere" (to fit the mesh in
+        an unit sphere). Default is None.
 
     off_surface_sdf: number, optional
         Value to replace the SDF calculated by `sample_mesh` for points with
@@ -36,32 +97,51 @@ class PointCloud(Dataset):
     & Wetzstein, G. (2020). Implicit Neural Representations with Periodic
     Activation Functions. ArXiv. Retrieved from http://arxiv.org/abs/2006.09661
     """
-    def __init__(self, mesh_path, samples_on_surface, samples_off_surface,
+    def __init__(self, mesh_path, samples_on_surface, scaling=None,
                  off_surface_sdf=None):
         super().__init__()
 
         self.samples_on_surface = samples_on_surface
+        self.off_surface_sdf = off_surface_sdf
 
         mesh = trimesh.load(mesh_path)
-        mesh = scale_to_unit_sphere(mesh)
-        self.point_cloud = torch.from_numpy(
-            sample_mesh(mesh, samples_on_surface, samples_off_surface)
-        )
-        if off_surface_sdf is not None:
-            self.point_cloud[(self.samples_on_surface + 1):, -1] = off_surface_sdf
+        if scaling is not None:
+            if scaling == "bbox":
+                mesh = scale_to_unit_cube(mesh)
+            elif scaling == "sphere":
+                mesh = scale_to_unit_sphere(mesh)
+            else:
+                raise ValueError("Invalid scaling option.")
 
-        # Since we can't guarantee that the number of off-surface samples
-        # requested will be respected, we set their number to the actual number
-        # of samples obtained. We will fix the sampling algorithm in the future
-        # to fix this.
-        self.samples_off_surface = self.point_cloud.shape[0] - samples_on_surface
+        self.mesh = mesh
+        self.point_cloud = get_surface_point_cloud(
+            mesh,
+            surface_point_method="scan",
+            bounding_radius=1,
+            calculate_normals=True
+        )
+        self.surface_samples = _sample_on_surface(
+            mesh,
+            samples_on_surface,
+            sample_vertices=True
+        )
 
     def __len__(self):
-        return self.samples_on_surface + self.samples_off_surface
+        return self.samples_on_surface
 
     def __getitem__(self, idx):
+        if idx in range(self.surface_samples.size(0)):
+            return {
+                "coords": self.surface_samples[idx, :3].float(),
+                "normals": self.surface_samples[idx, 3:6].float(),
+                "sdf": self.surface_samples[idx, -1].float(),
+            }
+
+        sample = _sample_domain(self.point_cloud, 1)
+        if self.off_surface_sdf is not None:
+            sample[0, -1] = self.off_surface_sdf
         return {
-            "coords": self.point_cloud[idx, :3].float(),
-            "normals": self.point_cloud[idx, 3:6].float(),
-            "sdf": self.point_cloud[idx, -1].float(),
+            "coords": sample[0, :3].float(),
+            "normals": sample[0, 3:6].float(),
+            "sdf": sample[0, -1].float(),
         }
