9d8fb8bd70b3c10f45fd005a63c17d7bcf89d48c
Guilherme Schardong <guilherme.schardong@isr.uc.pt>
2023-03-03 17:15:27 +0000

    Adding a setup script to install the package locally.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+
+setup(
+    name="i4d",
+    version="0.0.1",
+    description="Neural Implicit Surfaces in Higher Dimensions",
+    author="The Deep Implicits Group",
+    packages=["."],
+)
