06063e6f5464115de238b5cdf7e1ff025d84ee7a
Guilherme Schardong <guilherme.schardong@isr.uc.pt>
2023-07-19 16:03:03 +0100

    Minor fixes.

--- tools/reconstruct.py
+++ tools/reconstruct.py
@@ -50,9 +50,8 @@ if __name__ == "__main__":
     model = model.to(device)
     print(model)
     print(f"Running marching cubes running with resolution {args.resolution}")
-    
-    times = [float(t) for t in args.times]
 
+    times = [float(t) for t in args.times]
     reconstruct_at_times(model, times, out_dir, device=device)
 
     print("Done")
